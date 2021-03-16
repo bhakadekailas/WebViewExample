de342a334ca1301b06a53d90995cf92e5a26b1dd
bhakadekailas@belebentech.com <bhakadekailas@belebentech.com>
2021-03-16 23:30:48 +0530

    Webview Project Added.

---
+++ app/src/main/java/hakadekailas/webviewdemo/MainActivity.java
@@ -0,0 +1,22 @@
+package hakadekailas.webviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.clearCache(true);
+        webView.clearHistory();
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.griffyn.io/");
+    }
+}
